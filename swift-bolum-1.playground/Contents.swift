deb1fdf1e1454bd6fbce4e27d73cba516069e307
Çağdaş Ekşi <sirin.eksi@hotmail.com>
2022-02-28 13:21:00 +0300

    initial commit

---
+++ swift-bolum-1.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+//DEĞİŞKENLER
+
+var sayi : Int; // Tam sayıları olan değerleri ifade eder.
+var kiraGideri , yiyecekGideri : Float; // Ondalıklı sayıları ifade eder.
+var kaydettiMi : Bool; // True ve False veri tipini ifade eder.
+var x : Int = 1
+var y : Int = 2
+var d : Int = 1
+var e : Int = 1
+var f : Int = 1
+var g : Int = 1
+var k : Int = 1
+var u : Int = 1
+var p : Int = 1
+var m : Bool
+var n : Bool
+var l : Bool
+var o : Bool
+
+// Değişkenlere tanımlandıkları sırada bir ilk değer atanabilir.
+
+var sayı : Int = 1;
+var okulGideri : Float = 1500.0, servisGideri : Float = 800.0;
+var kaydettıMı : Bool = true;
+
+// Temel değişken tipleri
+
+// Int: Tamsayıları ifade eder
+// Bool: True ve False gibi mantıksal değerleri geri döndürür.
+// Float: Reel sayıları hafızada tutar.
+// Double: Float veri tipinden daha büyük reel sayıları tutmak için kullanılar.Genel olarak float veri tipine göre 2 kat daha fazla hafıza kaplar.
+
+// İŞLEMCİLER
+
+// 1. Aritmetik işlemciler
+
+// standart 4 temel işleme karşılık gelir; toplama,çıkarma,bölme,çarpma.
+//Beşinci aritmetik işlemci ise modüler aritmetik konusunda öğrenilen mod işlemcisidir.Birinci sayıyı ikinci sayıya bölündüğünde kalanı verir.
+// Toplama,çıkarma,çarpma,bölme işlemcileri Int,float,double veri tiplerine uygulanır, mod işlemcisi sadece Int veri tipine uygulanır.
+
+var a : Int = 5, b : Int = 3, c : Int;
+c = a / b;  // Int tipinde iki değişken birbirine bölünürse sonuç yine tam sayı olur. (1)
+
+// 2. Karşılaştırma ve mantıksal işlemciler
+
+x == y // x eşittir y
+x != y // X eşit değildir y
+x > y  // x büyüktür y
+x < y  // x küçüktür y
+x >= y // x büyüktür veya eşittir y
+x <= y // x küçüktür veya eşittir y
+m && n // x ve y
+l || o // v veya y
+
+
+// 3. Atama işlemcileri
+
+c += 7 // c=c+7 işlemini ifade eder.
+d -= 4 // d=d-4 işlemini ifade eder.
+e *= 5 // e=e*5 işlemini ifade eder.
+f /= 3 // f=f/3 işlemini ifade eder.
+
+// 4.Artırma ve azaltma işlemcileri
+
+c ++ // c=c+1 işlemini ifade eder.
+b -- // b=b-1 işlemini ifade eder.
+
+// KOŞULLU YAPILAR
+
+//1. If-Else
+
+  if k == 4 {
+      u += 1
+      p += u;
+    }
+
+
+
+
+
---
+++ swift1/swift1/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  swift1
+//
+//  Created by Çağdaş Ekşi on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var x : Int = 1
+        var y : Int = 2
+        var d : Int = 1
+        var e : Int = 1
+        var f : Int = 1
+        var g : Int = 1
+        var k : Int = 1
+        var u : Int = 1
+        var p : Int = 1
+        var z : Int = 1
+        var m : Bool
+        var n : Bool
+        var l : Bool
+        var o : Bool
+        
+    
+        //DEĞİŞKENLER
+
+        var sayi : Int; // Tam sayıları olan değerleri ifade eder.
+        var kiraGideri , yiyecekGideri : Float; // Ondalıklı sayıları ifade eder.
+        var kaydettiMi : Bool; // True ve False veri tipini ifade eder.
+        
+        // Değişkenlere tanımlandıkları sırada bir ilk değer atanabilir.
+
+        var sayı : Int = 1;
+        var okulGideri : Float = 1500.0, servisGideri : Float = 800.0;
+        var kaydettıMı : Bool = true;
+
+        // Temel değişken tipleri
+
+        // Int: Tamsayıları ifade eder
+        // Bool: True ve False gibi mantıksal değerleri geri döndürür.
+        // Float: Reel sayıları hafızada tutar.
+        // Double: Float veri tipinden daha büyük reel sayıları tutmak için kullanılar.Genel olarak float veri tipine göre 2 kat daha fazla hafıza kaplar.
+
+        // İŞLEMCİLER
+
+        // 1. Aritmetik işlemciler
+
+        // standart 4 temel işleme karşılık gelir; toplama,çıkarma,bölme,çarpma.
+        //Beşinci aritmetik işlemci ise modüler aritmetik konusunda öğrenilen mod işlemcisidir.Birinci sayıyı ikinci sayıya bölündüğünde kalanı verir.
+        // Toplama,çıkarma,çarpma,bölme işlemcileri Int,float,double veri tiplerine uygulanır, mod işlemcisi sadece Int veri tipine uygulanır.
+
+        var a : Int = 5, b : Int = 3, c : Int;
+        c = a / b;  // Int tipinde iki değişken birbirine bölünürse sonuç yine tam sayı olur. (1)
+
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
