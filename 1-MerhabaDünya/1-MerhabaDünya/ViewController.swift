d1f7ebc4ec36bc85a8332ff72b7fa82d468a2c51
Çağdaş Ekşi <sirin.eksi@hotmail.com>
2022-02-28 23:51:57 +0300

    initial commit

---
+++ 1-MerhabaDünya/1-MerhabaDünya/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  1-MerhabaDünya
+//
+//  Created by Çağdaş Ekşi on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var appleLogo: UIImageView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
